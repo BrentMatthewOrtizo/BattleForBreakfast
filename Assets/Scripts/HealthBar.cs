b12ebca8d2cb48ce7c5a75c95e6c051f97327dab
Brent Matthew Ortizo <ortizo@chapman.edu>
2025-05-15 13:52:08 -0700

    Created health UI

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        SetHealth(health);
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+    
+}
