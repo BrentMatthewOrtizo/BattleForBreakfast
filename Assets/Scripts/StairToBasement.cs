1865149e7e42107e43af9f6d55422fe1e61bf1d5
mjmpmanny <chapmanny2785@gmail.com>
2025-05-15 17:26:22 -0700

    Made first basement part for game map

--- Assets/Scripts/StairToBasement.cs
+++ Assets/Scripts/StairToBasement.cs
@@ -1,20 +1,24 @@
-using System;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class StairToBasement : MonoBehaviour, IInteractable
 {
 
     public GameObject player;
+    public PolygonCollider2D mapBoundary;
+    public CinemachineConfiner2D confiner;
     
-    private Vector2 teleportLocation = new Vector2(0.78f, -7.46f);
+    private Vector2 teleportLocation = new Vector2(55.55f, -6.07f);
 
     public void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
     }
 
     public void Interact()
     {
+        confiner.BoundingShape2D = mapBoundary;
         player.transform.position = teleportLocation;
     }
 
